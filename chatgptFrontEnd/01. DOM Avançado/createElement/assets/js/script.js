1ed59adbaaace2c3d3d220dcc6fc49bd26c345ab
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-11-20 21:49:48 -0300

    feat(success): Desenvolver createElement para criar elemento HTML

--- chatgptFrontEnd/01. DOM Avançado/createElement/assets/js/script.js
+++ chatgptFrontEnd/01. DOM Avançado/createElement/assets/js/script.js
@@ -1,10 +1,19 @@
-window.addEventListener('load', () =>{
-	const novoElementoH1 = document.createElement('h1');
+window.addEventListener('load', () => {
+	const novoElementoCriado = document.createElement('h1');
 
-	novoElementoH1.textContent = 'Este texto foi criado com createElement';
+	novoElementoCriado.textContent = 'Este é o conteúdo do novo elemento criado com createElement.';
 
-	const divAtual = document.querySelector('main div');
+	const elementoJaExistente = document.querySelector('main div');
 
-	divAtual.before(novoElementoH1);
+	/*Before - Adiciona o elemento como irmão antes do elemento alvo*/
+	elementoJaExistente.before(novoElementoCriado);
 
+	/*After - Adiciona o elemento como irmão depois do elemento alvo
+	elementoJaExistente.after(novoElementoCriado);*/
+
+	/*appendChild - Adiciona o elemento como ultimo filho dentro do elemento alvo
+	elementoJaExistente.appendChild(novoElementoCriado);*/
+
+	/*prepend - Adiciona o elemento como primeiro filho dentro do elemento alvo
+	elementoJaExistente.prepend(novoElementoCriado);*/
 });
