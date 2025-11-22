a4e624463e9320b257f703d6cf8c284f1e4102cf
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-11-22 13:59:27 -0300

    feat(start): Desenvolver removeChild ou remove para excluir elemento HTML

---
+++ chatgptFrontEnd/01. DOM Avançado/removeChild/assets/js/script.js
@@ -0,0 +1,10 @@
+const parent = document.getElementById('parent');
+
+const child = document.getElementById('child');
+
+/*const throwawayNode = parent.removeChild(child);*/
+
+
+/*Outra maneira de remover um elemento sem utilizar a função removeChild()*/
+
+child.remove();
