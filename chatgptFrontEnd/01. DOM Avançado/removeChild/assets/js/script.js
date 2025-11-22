9ceddbd441e30550c5151258507b86fc33a44037
Carlos Eduardo Silva <carlos.eduardo-silva@hotmail.com>
2025-11-22 17:56:24 -0300

    feat(attempt 1): Desenvolver removeChild para excluir elemento HTML
    Foi utilizado minha lógica de tentativa e erro para controlar os itens adicionados e excluidos

--- chatgptFrontEnd/01. DOM Avançado/removeChild/assets/js/script.js
+++ chatgptFrontEnd/01. DOM Avançado/removeChild/assets/js/script.js
@@ -1,10 +1,33 @@
-const parent = document.getElementById('parent');
+const produtos = ["banana","laranja","cereja","morango","manga","cenoura","goiaba","uva","acerola","beringela"];
+let contador = 1, ultimoContador;
 
-const child = document.getElementById('child');
+/*Adicionar Item na Lista de maneira aleatória pega um dos produtos e adiciona na lista quando clica no botão.*/
 
-/*const throwawayNode = parent.removeChild(child);*/
+document.querySelector('main button[name="adicionar"]').addEventListener('click', () => {
+	
+	const novoItem = document.createElement('li');
+	
+	novoItem.textContent = `${contador} - ` + produtos[Math.trunc(Math.random(10)*10)];
+	
+	document.querySelector('main ul').appendChild(novoItem);
 
+	contador +=1;
 
-/*Outra maneira de remover um elemento sem utilizar a função removeChild()*/
+	ultimoContador = contador;
+});
 
-child.remove();
+/*Exclui Item da Lista a partir do ultimo item até o primeiro item da lista com controle do ultimoContador para saber qual vai ser o próximo item da lista a partir do ultimo item que foi excluído*/
+
+document.querySelector('main button[name="excluir"]').addEventListener('click', () => {
+
+	const lista = document.querySelector('main ul');
+	
+	if(lista.firstChild){
+		lista.removeChild(lista.lastElementChild);
+		ultimoContador -= 1;
+		contador = ultimoContador;
+	} else{
+		contador = 1;
+	}
+
+});
